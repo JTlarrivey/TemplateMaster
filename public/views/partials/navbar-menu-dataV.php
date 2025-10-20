19e1e6c1398ec17988fbd1686da95cf51b6e7a6b
Larry <joaqlarrivey@gmail.com>
2025-10-20 12:41:51 -0300

    fixeando el navbar menu

--- public/views/partials/navbar-menu-dataV.php
+++ public/views/partials/navbar-menu-dataV.php
@@ -1,191 +1,191 @@
    <li class="nav-title"><span>Data Visualization</span></li>
-                        <li class="nav-item ">
-                            <a href="#" title="Apex Charts" data-filter-tags="">
-                                <svg class="sa-icon">
-                                    <use href="/converseLarry/public/assets/img/sprite.svg#pie-chart"></use>
-                                </svg>
-                                <span class="nav-link-text" data-i18n="">Apex Charts</span>
-                            </a>
-                            <ul>
-                                <li class="">
-                                    <a href="apexareachart.php">
-                                        <span class="nav-link-text" data-i18n="">Area Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexbarchart.html">
-                                        <span class="nav-link-text" data-i18n="">Bar Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexboxwhiskerchart.html">
-                                        <span class="nav-link-text" data-i18n="">Box & Whisker Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexbubblechart.html">
-                                        <span class="nav-link-text" data-i18n="">Bubble Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexcandlestickchart.html">
-                                        <span class="nav-link-text" data-i18n="">Candlestick Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexcolumnchart.html">
-                                        <span class="nav-link-text" data-i18n="">Column Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexfunnelchart.html">
-                                        <span class="nav-link-text" data-i18n="">Funnel Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexheatmapchart.html">
-                                        <span class="nav-link-text" data-i18n="">Heatmap Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexlinechart.html">
-                                        <span class="nav-link-text" data-i18n="">Line Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexmixedcombochart.html">
-                                        <span class="nav-link-text" data-i18n="">Mixed/Combo Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexpiedonut.html">
-                                        <span class="nav-link-text" data-i18n="">Pie/Donuts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexpolarareachart.html">
-                                        <span class="nav-link-text" data-i18n="">Polar Area Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexradarchart.html">
-                                        <span class="nav-link-text" data-i18n="">Radar Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexradialbarscirclechart.html">
-                                        <span class="nav-link-text" data-i18n="">RadialBars/Circle Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexrangeareachart.html">
-                                        <span class="nav-link-text" data-i18n="">Range Area Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexscatterchart.html">
-                                        <span class="nav-link-text" data-i18n="">Scatter Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexslopechart.html">
-                                        <span class="nav-link-text" data-i18n="">Slope Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apexsparkline.html">
-                                        <span class="nav-link-text" data-i18n="">Sparklines</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apextimelinechart.html">
-                                        <span class="nav-link-text" data-i18n="">Timeline Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="apextreemapchart.html">
-                                        <span class="nav-link-text" data-i18n="">Treemap Charts</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item ">
-                            <a href="#" title="Data Bites" data-filter-tags="">
-                                <svg class="sa-icon">
-                                    <use href="/converseLarry/public/assets/img/sprite.svg#grid"></use>
-                                </svg>
-                                <span class="nav-link-text" data-i18n="">Data Bites</span>
-                            </a>
-                            <ul>
-                                <li class="">
-                                    <a href="peitycharts.html">
-                                        <span class="nav-link-text" data-i18n="">Peity Charts</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="streamline.html">
-                                        <span class="nav-link-text" data-i18n="">Streamline</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="easypiechart.html">
-                                        <span class="nav-link-text" data-i18n="">Easy Pie Chart</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="">
-                            <a href="fullcalendar.html">
-                                <svg class="sa-icon">
-                                    <use href="/converseLarry/public/assets/img/sprite.svg#calendar"></use>
-                                </svg>
-                                <span class="nav-link-text">Full Calendar</span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a href="#" title="Smart Tables" data-filter-tags="">
-                                <svg class="sa-icon">
-                                    <use href="/converseLarry/public/assets/img/sprite.svg#database"></use>
-                                </svg>
-                                <span class="nav-link-text" data-i18n="">Smart Tables</span>
-                                <span class="badge bg-primary-500 badge-end">1.2.7</span>
-                            </a>
-                            <ul>
-                                <li class="">
-                                    <a href="smarttablesminimal.html">
-                                        <span class="nav-link-text" data-i18n="">Minimal Settings</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="smarttablesresponsive.html">
-                                        <span class="nav-link-text" data-i18n="">Responsive Settings</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="smarttablesimportexportdata.html">
-                                        <span class="nav-link-text" data-i18n="">Import & Export Data</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="smarttablesjsonsource.html">
-                                        <span class="nav-link-text" data-i18n="">JSON Data Source</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="smarttablesmanagerecords.html">
-                                        <span class="nav-link-text" data-i18n="">Manage Records</span>
-                                        <span class="badge bg-warning text-dark badge-end">New</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="smarttablesfuzzymatching.html">
-                                        <span class="nav-link-text" data-i18n="">Fuzzy Matching</span>
-                                    </a>
-                                </li>
-                                <li class="">
-                                    <a href="smarttablesserverside.html">
-                                        <span class="nav-link-text" data-i18n="">Server-Side Mode</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
+   <li class="nav-item ">
+       <a href="#" title="Apex Charts" data-filter-tags="">
+           <svg class="sa-icon">
+               <use href="/converseLarry/public/assets/img/sprite.svg#pie-chart"></use>
+           </svg>
+           <span class="nav-link-text" data-i18n="">Apex Charts</span>
+       </a>
+       <ul>
+           <li class="">
+               <a href="apexareachart.html">
+                   <span class="nav-link-text" data-i18n="">Area Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexbarchart.html">
+                   <span class="nav-link-text" data-i18n="">Bar Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexboxwhiskerchart.html">
+                   <span class="nav-link-text" data-i18n="">Box & Whisker Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexbubblechart.html">
+                   <span class="nav-link-text" data-i18n="">Bubble Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexcandlestickchart.html">
+                   <span class="nav-link-text" data-i18n="">Candlestick Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexcolumnchart.html">
+                   <span class="nav-link-text" data-i18n="">Column Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexfunnelchart.html">
+                   <span class="nav-link-text" data-i18n="">Funnel Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexheatmapchart.html">
+                   <span class="nav-link-text" data-i18n="">Heatmap Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexlinechart.html">
+                   <span class="nav-link-text" data-i18n="">Line Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexmixedcombochart.html">
+                   <span class="nav-link-text" data-i18n="">Mixed/Combo Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexpiedonut.html">
+                   <span class="nav-link-text" data-i18n="">Pie/Donuts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexpolarareachart.html">
+                   <span class="nav-link-text" data-i18n="">Polar Area Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexradarchart.html">
+                   <span class="nav-link-text" data-i18n="">Radar Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexradialbarscirclechart.html">
+                   <span class="nav-link-text" data-i18n="">RadialBars/Circle Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexrangeareachart.html">
+                   <span class="nav-link-text" data-i18n="">Range Area Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexscatterchart.html">
+                   <span class="nav-link-text" data-i18n="">Scatter Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexslopechart.html">
+                   <span class="nav-link-text" data-i18n="">Slope Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apexsparkline.html">
+                   <span class="nav-link-text" data-i18n="">Sparklines</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apextimelinechart.html">
+                   <span class="nav-link-text" data-i18n="">Timeline Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="apextreemapchart.html">
+                   <span class="nav-link-text" data-i18n="">Treemap Charts</span>
+               </a>
+           </li>
+       </ul>
+   </li>
+   <li class="nav-item ">
+       <a href="#" title="Data Bites" data-filter-tags="">
+           <svg class="sa-icon">
+               <use href="/converseLarry/public/assets/img/sprite.svg#grid"></use>
+           </svg>
+           <span class="nav-link-text" data-i18n="">Data Bites</span>
+       </a>
+       <ul>
+           <li class="">
+               <a href="peitycharts.html">
+                   <span class="nav-link-text" data-i18n="">Peity Charts</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="streamline.html">
+                   <span class="nav-link-text" data-i18n="">Streamline</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="easypiechart.html">
+                   <span class="nav-link-text" data-i18n="">Easy Pie Chart</span>
+               </a>
+           </li>
+       </ul>
+   </li>
+   <li class="">
+       <a href="fullcalendar.html">
+           <svg class="sa-icon">
+               <use href="/converseLarry/public/assets/img/sprite.svg#calendar"></use>
+           </svg>
+           <span class="nav-link-text">Full Calendar</span>
+       </a>
+   </li>
+   <li class="nav-item ">
+       <a href="#" title="Smart Tables" data-filter-tags="">
+           <svg class="sa-icon">
+               <use href="/converseLarry/public/assets/img/sprite.svg#database"></use>
+           </svg>
+           <span class="nav-link-text" data-i18n="">Smart Tables</span>
+           <span class="badge bg-primary-500 badge-end">1.2.7</span>
+       </a>
+       <ul>
+           <li class="">
+               <a href="smarttablesminimal.html">
+                   <span class="nav-link-text" data-i18n="">Minimal Settings</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="smarttablesresponsive.html">
+                   <span class="nav-link-text" data-i18n="">Responsive Settings</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="smarttablesimportexportdata.html">
+                   <span class="nav-link-text" data-i18n="">Import & Export Data</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="smarttablesjsonsource.html">
+                   <span class="nav-link-text" data-i18n="">JSON Data Source</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="smarttablesmanagerecords.html">
+                   <span class="nav-link-text" data-i18n="">Manage Records</span>
+                   <span class="badge bg-warning text-dark badge-end">New</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="smarttablesfuzzymatching.html">
+                   <span class="nav-link-text" data-i18n="">Fuzzy Matching</span>
+               </a>
+           </li>
+           <li class="">
+               <a href="smarttablesserverside.html">
+                   <span class="nav-link-text" data-i18n="">Server-Side Mode</span>
+               </a>
+           </li>
+       </ul>
+   </li>
